6ad53699c4b741c33c2854f4674e4f01db583556
smalois <saloisi1@binghamton.edu>
2021-06-30 21:45:23 -0400

    Correct column tags on guestlist CSV

--- guest/management/commands/update_guestlist.py
+++ guest/management/commands/update_guestlist.py
@@ -16,14 +16,16 @@ class Command(BaseCommand):
     def save_guests_to_db(self, csv_reader):                                                                                                                                                   
         h = hashlib.md5()                                                                                                                                                                      
         for row in csv_reader:                                                                                                                                                                 
-            concat_name = row['first_name'] + row['last_name']                                                                                                                                 
+            first_name = row['First name']
+            last_name = row['Last name']
+            concat_name = row['First name'] + row['Last name']                                                                                                                                 
             h.update(str.encode(concat_name))                                                                                                                                                  
             pword = h.hexdigest()[0:8]                                                                                                                                                         
             u = User.objects.create_user(username=concat_name, password=pword)
             guest = Guest(user=u, hasVoted=False)
             guest.save()
             self.stdout.write(                                                                                                                                                                 
-                    self.style.SUCCESS("%s/%s" % (concat_name, pword)))                                                                                                                        
+                    self.style.SUCCESS("%s,%s,%s,1" % (first_name, last_name, pword)))                                                                                                                        
                                                                                                                                                                                                
                                                                                                                                                                                                
     def handle(self, *argrs, **options):                                                                                                                                                       
